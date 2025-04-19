a06965b64802e71a6a52e2f94a63299c8ec59f73
logles <lydialogles@gmail.com>
2025-04-19 13:41:53 -0600

    round 4

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -3,7 +3,7 @@ import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
 //import pythonQuestions from "./pythonQuestions.json" assert { type: "json" };
-import pythonQuestions from './pythonQuestions.json' as { type: "json" };
+import pythonQuestions from "./pythonQuestions.json"; // as { type: "json" };
 
 db.once("open", async () => {
   await cleanDB("Question", "questions");
