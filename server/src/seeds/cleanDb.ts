edd2ad41959d070822ae19d432262e78464ed37e
logles <lydialogles@gmail.com>
2025-04-19 13:10:23 -0600

    fix seeds error

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,16 +1,18 @@
-import models from '../models/index.js';
-import db from '../config/connection.js';
+import models from "../models/index.js";
+import db from "../config/connection.js";
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    let modelExists = await models[modelName].db.db
+      ?.listCollections({
+        name: collectionName,
+      })
+      .toArray();
 
-    if (modelExists.length) {
+    if (modelExists?.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
     throw err;
   }
-}
+};
